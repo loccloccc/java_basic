ab36fc9a02d0faaae0931bc2d01b536ea1254c96
Dương Đức Lộc <locduong25051006@gmail.com>
2026-03-03 07:58:06 +0700

    miniPrj

--- Session16/src/miniPrj/Main.java
+++ Session16/src/miniPrj/Main.java
@@ -1,5 +1,7 @@
 package miniPrj;
 
+import java.util.List;
+
 public class Main {
     static void main(String[] args) {
         ProductRepository repository = new ProductRepository() {};
@@ -8,7 +10,25 @@ public class Main {
         repository.add(new FoodProduct("FP1","Mi cay",20000,5));
         repository.add(new FoodProduct("FP2","Sua chua",25000,10));
         // hien thi toan bo
+        List<Product> products = repository.findAll();
 
+        if (products != null && !products.isEmpty()) {
+            for (Product p : products) {
+                p.displayInfo();
+                System.out.println("Thành tiền: " + p.calculateFinalPrice());
+                System.out.println("----------------------------");
+            }
+        }
+
+        String searchId = "EP1";
+        Product found = repository.findById(searchId);
+
+        if (found != null) {
+            found.displayInfo();
+            System.out.println("Thành tiền: " + found.calculateFinalPrice());
+        } else {
+            System.out.println("Không tìm thấy sản phẩm!");
+        }
 
 
 
